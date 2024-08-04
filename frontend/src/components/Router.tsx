f14a3c2f4423b6cce1b33ec7151cabcc9e812ea4
SoyeonIm <imjoyy.i@gmail.com>
2024-08-04 20:29:28 +1200

    Add user authentication and post creation components

---
+++ frontend/src/components/Router.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+   import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+   import Home from './components/Home';
+   import Profile from './components/Profile';
+   import Login from './components/Login';
+   import Register from './components/Register';
+   import CreatePost from './components/CreatePost';
+
+   const AppRouter: React.FC = () => (
+     <Router>
+       <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/profile" component={Profile} />
+         <Route path="/login" component={Login} />
+         <Route path="/register" component={Register} />
+         <Route path="/create-post" component={CreatePost} />
+       </Switch>
+     </Router>
+   );
+
+   export default AppRouter;
